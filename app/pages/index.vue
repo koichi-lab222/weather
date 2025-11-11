8b715ebeb91b71b3e75fe30299b6678336b3c2a1
koichi-lab222 <243270140+koichi-lab222@users.noreply.github.com>
2025-11-11 16:33:47 +0900

    Add routing and add pref pages

---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup>
+const config = useRuntimeConfig()
+const endpoint = config.public.weatherEndpoint
+
+async function onButtonClick() {
+  console.log(endpoint+"&q=Tokyo")
+  const res = await $fetch(endpoint+"&q=Tokyo")
+  console.log(res)
+}
+
+async function onDummyButtonClick() {
+  const url = "http://localhost:3000/dummy.json?dummy=dummy"
+  const res = await $fetch(url)
+  console.log(res)
+}
+</script>
+
+<template>
+  <div>
+    <button @click="onButtonClick">tokyo tenki</button>
+    <button @click="onDummyButtonClick">tokyo tenki(dummy)</button>
+  </div>
+</template>
