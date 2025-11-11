edc1ba32808a79249f09dbf3d643aa03aee11ad1
koichi-lab222 <243270140+koichi-lab222@users.noreply.github.com>
2025-11-11 15:55:30 +0900

    Try api access and add dummy api data

--- app/app.vue
+++ app/app.vue
@@ -2,13 +2,22 @@
 const config = useRuntimeConfig()
 const endpoint = config.public.weatherEndpoint
 
-function onButtonClick() {
-  console.log(endpoint)
+async function onButtonClick() {
+  console.log(endpoint+"&q=Tokyo")
+  const res = await $fetch(endpoint+"&q=Tokyo")
+  console.log(res)
+}
+
+async function onDummyButtonClick() {
+  const url = "http://localhost:3000/dummy.json?dummy=dummy"
+  const res = await $fetch(url)
+  console.log(res)
 }
 </script>
 
 <template>
   <div>
-    <button @click="onButtonClick">tenki</button>
+    <button @click="onButtonClick">tokyo tenki</button>
+    <button @click="onDummyButtonClick">tokyo tenki(dummy)</button>
   </div>
 </template>
