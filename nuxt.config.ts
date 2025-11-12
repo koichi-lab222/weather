31ed10fab05ea8e092fa964dd8594c47e5527ac2
koichi-lab222 <243270140+koichi-lab222@users.noreply.github.com>
2025-11-12 11:47:50 +0900

    Set default value of weatherEndpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      weatherEndpoint: '',
+      weatherEndpoint: '/dummy.json?dummy=dummy',
     }
   },
 })
