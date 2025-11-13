fe6823fc1a229598d1947ac4b4a68cb63785bfa6
koichi-lab222 <243270140+koichi-lab222@users.noreply.github.com>
2025-11-13 22:48:28 +0900

    Prettify index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,10 +3,35 @@ const config = useRuntimeConfig()
 const endpoint = config.public.weatherEndpoint
 const prefs = config.public.prefs
 
+useHead({
+  title: "各地の天気"
+})
 </script>
 
 <template>
+  <h1 class="top-title">各地の天気</h1>
   <div>
-    <div v-for="pref in prefs"><a v-bind:href="'/pref/'+pref.q">{{ pref.name }}</a></div>
+    <div class="pref-link" v-for="pref in prefs"><a v-bind:href="'/pref/'+pref.q">{{ pref.name }}の天気</a></div>
   </div>
 </template>
+
+<style scoped>
+.top-title {
+  text-align: center;
+}
+.pref-link {
+  font-size: 24px;
+  text-align: center;
+  background: linear-gradient(to right, #00c, #08f);
+  margin: 5px;
+}
+.pref-link:hover {
+  opacity: 0.8;
+}
+.pref-link a {
+  display: block;
+  text-decoration: none;
+  color: #fff;
+  font-weight: bold;
+}
+</style>
